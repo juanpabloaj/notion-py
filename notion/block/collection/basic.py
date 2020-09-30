68d30bacab690e30e10cc663f9afe12154297a4c
Artur Tamborski <tamborskiartur@gmail.com>
2020-09-30 02:23:23 +0200

    collection/basic: use new converters

--- notion/block/collection/basic.py
+++ notion/block/collection/basic.py
@@ -1,21 +1,14 @@
-from datetime import datetime, date
-from random import choice
 from typing import Optional
-from uuid import uuid1
 
 from notion.block.basic import PageBlock, Block
 from notion.block.children import Templates
-from notion.block.collection.common import NotionDate
 from notion.block.collection.media import CollectionViewBlock
 from notion.block.collection.query import CollectionQuery
 from notion.block.collection.view import CalendarView
+from notion.converter import PythonToNotionConverter, NotionToPythonConverter
 from notion.maps import markdown_field_map, field_map
-from notion.markdown import notion_to_markdown, markdown_to_notion
-from notion.operations import build_operation
 from notion.utils import (
     slugify,
-    add_signed_prefix_as_needed,
-    remove_signed_prefix_as_needed,
 )
 
 
@@ -39,9 +32,10 @@ class CollectionBlock(Block):
     @property
     def templates(self) -> Templates:
         if not self._templates:
-            template_ids = self.get("template_pages", [])
-            self._client.refresh_records(block=template_ids)
+            template_pages = self.get("template_pages", [])
+            self._client.refresh_records(block=template_pages)
             self._templates = Templates(parent=self)
+
         return self._templates
 
     def get_schema_properties(self) -> list:
@@ -57,8 +51,7 @@ class CollectionBlock(Block):
         properties = []
         for block_id, item in self.get("schema").items():
             slug = slugify(item["name"])
-            prop = {"id": block_id, "slug": slug, **item}
-            properties.append(prop)
+            properties.append({"id": block_id, "slug": slug, **item})
 
         return properties
 
@@ -206,27 +199,34 @@ class CollectionRowBlock(PageBlock):
 
     @property
     def schema(self):
-        return [
-            prop
-            for prop in self.collection.get_schema_properties()
-            if prop["type"] not in ["formula", "rollup"]
-        ]
+        props = self.collection.get_schema_properties()
+        return [p for p in props if p["type"] not in ["formula", "rollup"]]
 
     def __getattr__(self, attname):
         return self.get_property(attname)
 
-    def __setattr__(self, attname, value):
-        if attname.startswith("_"):
-            # we only allow setting of new non-property attributes that start with "_"
-            super().__setattr__(attname, value)
-        elif attname in self._get_property_slugs():
-            self.set_property(attname, value)
-        elif slugify(attname) in self._get_property_slugs():
-            self.set_property(slugify(attname), value)
-        elif hasattr(self, attname):
-            super().__setattr__(attname, value)
-        else:
-            raise AttributeError("Unknown property: '{}'".format(attname))
+    def __setattr__(self, name, value):
+        if name.startswith("_"):
+            # we only allow setting of new non-property
+            # attributes that start with "_"
+            super().__setattr__(name, value)
+            return
+
+        slugs = self._get_property_slugs()
+        if name in slugs:
+            self.set_property(name, value)
+            return
+
+        slugged_name = slugify(name)
+        if slugged_name in slugs:
+            self.set_property(slugged_name, value)
+            return
+
+        if hasattr(self, name):
+            super().__setattr__(name, value)
+            return
+
+        raise AttributeError(f"Unknown property: '{name}'")
 
     def _get_property_slugs(self):
         slugs = [prop["slug"] for prop in self.schema]
@@ -237,28 +237,34 @@ class CollectionRowBlock(PageBlock):
     def __dir__(self):
         return self._get_property_slugs() + dir(super())
 
-    def get_property(self, identifier):
-        prop = self.collection.get_schema_property(identifier)
+    def _get_property(self, name):
+        prop = self.collection.get_schema_property(name)
         if prop is None:
-            raise AttributeError(
-                "Object does not have property '{}'".format(identifier)
-            )
+            raise AttributeError(f"Object does not have property '{name}'")
 
-        val = self.get(["properties", prop["id"]])
+        prop_id = prop["id"]
+        value = self.get(f"properties.{prop_id}")
+        return value, prop
 
-        return self._convert_notion_to_python(val, prop)
+    def get_property(self, name):
+        return self._convert_notion_to_python(*self._get_property(name))
 
-    def get_mentioned_pages_on_property(self, identifier):
-        prop = self.collection.get_schema_property(identifier)
-        if prop is None:
-            raise AttributeError(
-                "Object does not have property '{}'".format(identifier)
-            )
-        val = self.get(["properties", prop["id"]])
-        return self._convert_mentioned_pages_to_python(val, prop)
+    def get_all_properties(self):
+        props = {}
+        for prop in self.schema:
+            prop_id = slugify(prop["name"])
+            props[prop_id] = self.get_property(prop_id)
+
+        return props
+
+    def set_property(self, name, value):
+        _, prop = self._get_property(name)
+        self.set(*self._convert_python_to_notion(value, prop, name))
+
+    def get_mentioned_pages_on_property(self, name):
+        return self._convert_mentioned_pages_to_python(*self._get_property(name))
 
     def _convert_diff_to_changelist(self, difference, old_val, new_val):
-
         changed_props = set()
         changes = []
         remaining = []
@@ -267,7 +273,7 @@ class CollectionRowBlock(PageBlock):
             operation, path, values = d
             path = path.split(".") if isinstance(path, str) else path
             if path and path[0] == "properties":
-                if len(path) > 1:
+                if len(path):
                     changed_props.add(path[1])
                 else:
                     for item in values:
@@ -289,14 +295,14 @@ class CollectionRowBlock(PageBlock):
             remaining, old_val, new_val
         )
 
-    def _convert_mentioned_pages_to_python(self, val, prop):
+    def _convert_mentioned_pages_to_python(self, value, prop):
         if not prop["type"] in ["title", "text"]:
             raise TypeError(
                 "The property must be an title or text to convert mentioned pages to Python."
             )
 
         pages = []
-        for i, part in enumerate(val):
+        for i, part in enumerate(value):
             if len(part) == 2:
                 for format in part[1]:
                     if "p" in format:
@@ -305,205 +311,12 @@ class CollectionRowBlock(PageBlock):
         return pages
 
     def _convert_notion_to_python(self, val, prop):
-        # TODO: REWRITE THIS MONSTROSITY !!
+        _, value = NotionToPythonConverter.convert(value=val, prop=prop, block=self)
+        return value
 
-        if prop["type"] in ["title", "text"]:
-            for i, part in enumerate(val):
-                if len(part) == 2:
-                    for format in part[1]:
-                        if "p" in format:
-                            page = self._client.get_block(format[1])
-                            val[i] = [
-                                "["
-                                + page.icon
-                                + " "
-                                + page.title
-                                + "]("
-                                + page.get_browseable_url()
-                                + ")"
-                            ]
-
-            val = notion_to_markdown(val) if val else ""
-        if prop["type"] in ["number"]:
-            if val is not None:
-                val = val[0][0]
-                if "." in val:
-                    val = float(val)
-                else:
-                    val = int(val)
-        if prop["type"] in ["select"]:
-            val = val[0][0] if val else None
-        if prop["type"] in ["multi_select"]:
-            val = [v.strip() for v in val[0][0].split(",")] if val else []
-        if prop["type"] in ["person"]:
-            val = (
-                [self._client.get_user(item[1][0][1]) for item in val if item[0] == "‣"]
-                if val
-                else []
-            )
-        if prop["type"] in ["email", "phone_number", "url"]:
-            val = val[0][0] if val else ""
-        if prop["type"] in ["date"]:
-            val = NotionDate.from_notion(val)
-        if prop["type"] in ["file"]:
-            val = (
-                [
-                    add_signed_prefix_as_needed(item[1][0][1], client=self._client)
-                    for item in val
-                    if item[0] != ","
-                ]
-                if val
-                else []
-            )
-        if prop["type"] in ["checkbox"]:
-            val = val[0][0] == "Yes" if val else False
-        if prop["type"] in ["relation"]:
-            val = (
-                [
-                    self._client.get_block(item[1][0][1])
-                    for item in val
-                    if item[0] == "‣"
-                ]
-                if val
-                else []
-            )
-        if prop["type"] in ["created_time", "last_edited_time"]:
-            val = self.get(prop["type"])
-            val = datetime.utcfromtimestamp(val / 1000)
-        if prop["type"] in ["created_by", "last_edited_by"]:
-            val = self.get(prop["type"] + "_id")
-            val = self._client.get_user(val)
-
-        return val
-
-    def get_all_properties(self):
-        allprops = {}
-        for prop in self.schema:
-            propid = slugify(prop["name"])
-            allprops[propid] = self.get_property(propid)
-        return allprops
-
-    def set_property(self, identifier, val):
-
-        prop = self.collection.get_schema_property(identifier)
-        if prop is None:
-            raise AttributeError(
-                "Object does not have property '{}'".format(identifier)
-            )
-
-        path, val = self._convert_python_to_notion(val, prop, identifier=identifier)
-
-        self.set(path, val)
-
-    def _convert_python_to_notion(self, val, prop, identifier="<unknown>"):
-        # TODO: rewrite this terrible mess
-
-        if prop["type"] in ["title", "text"]:
-            if not val:
-                val = ""
-            if not isinstance(val, str):
-                raise TypeError(
-                    "Value passed to property '{}' must be a string.".format(identifier)
-                )
-            val = markdown_to_notion(val)
-        if prop["type"] in ["number"]:
-            if val is not None:
-                if not isinstance(val, float) and not isinstance(val, int):
-                    raise TypeError(
-                        "Value passed to property '{}' must be an int or float.".format(
-                            identifier
-                        )
-                    )
-                val = [[str(val)]]
-
-        if val and prop["type"] in ["select", "multi_select"]:
-            colors = [
-                "default",
-                "gray",
-                "brown",
-                "orange",
-                "yellow",
-                "green",
-                "blue",
-                "purple",
-                "pink",
-                "red",
-            ]
-            prop["options"] = prop.get("options", [])
-
-            valid_options = list([p["value"].lower() for p in prop["options"]])
-            if not isinstance(val, list):
-                val = [val]
-            schema_need_update = False
-            for vid, v in enumerate(val):
-                val[vid] = v = v.replace(",", "")
-                if v.lower() not in valid_options:
-                    schema_need_update = True
-                    prop["options"].append(
-                        {"id": str(uuid1()), "value": v, "color": choice(colors)}
-                    )
-                    valid_options.append(v.lower())
-            val = [[",".join(val)]]
-            if schema_need_update:
-                schema = self.collection.get("schema")
-                schema[prop["id"]] = prop
-                self.collection.set("schema", schema)
-        if prop["type"] in ["person"]:
-            userlist = []
-            if not isinstance(val, list):
-                val = [val]
-            for user in val:
-                user_id = user if isinstance(user, str) else user.id
-                userlist += [["‣", [["u", user_id]]], [","]]
-            val = userlist[:-1]
-        if prop["type"] in ["email", "phone_number", "url"]:
-            val = [[val, [["a", val]]]]
-        if prop["type"] in ["date"]:
-            if isinstance(val, date) or isinstance(val, datetime):
-                val = NotionDate(val)
-            if isinstance(val, NotionDate):
-                val = val.to_notion()
-            else:
-                val = []
-        if prop["type"] in ["file"]:
-            filelist = []
-            if not isinstance(val, list):
-                val = [val]
-            for url in val:
-                url = remove_signed_prefix_as_needed(url)
-                filename = url.split("/")[-1]
-                filelist += [[filename, [["a", url]]], [","]]
-            val = filelist[:-1]
-        if prop["type"] in ["checkbox"]:
-            if not isinstance(val, bool):
-                raise TypeError(
-                    "Value passed to property '{}' must be a bool.".format(identifier)
-                )
-            val = [["Yes" if val else "No"]]
-        if prop["type"] in ["relation"]:
-            pagelist = []
-            if not isinstance(val, list):
-                val = [val]
-            for page in val:
-                if isinstance(page, str):
-                    page = self._client.get_block(page)
-                pagelist += [["‣", [["p", page.id]]], [","]]
-            val = pagelist[:-1]
-        if prop["type"] in ["created_time", "last_edited_time"]:
-            val = int(val.timestamp() * 1000)
-            return prop["type"], val
-        if prop["type"] in ["created_by", "last_edited_by"]:
-            val = val if isinstance(val, str) else val.id
-            return prop["type"], val
-
-        return ["properties", prop["id"]], val
-
-    def remove(self):
-        # Mark the block as inactive
-        self._client.submit_transaction(
-            build_operation(
-                id=self.id, path=[], args={"alive": False}, command="update"
-            )
+    def _convert_python_to_notion(self, val, prop, name="<unknown>"):
+        return PythonToNotionConverter.convert(
+            name=name, value=val, prop=prop, block=self
         )
 
 
